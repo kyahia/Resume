43484b461e2227b9b7f4204cac2f68c55c320513
kyahia <96874471+kyahia@users.noreply.github.com>
2022-01-19 16:57:02 +0100

    v:1.0 Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelectorAll('a').forEach(link => link.setAttribute('target', '_blank'));
+
+const exps = document.querySelectorAll('section>div');
+// const projs = document.querySelectorAll('.project');
+
+exps.forEach(exp => {
+   exp.setAttribute('title', 'Click for details')
+   exp.addEventListener('click', activate)
+});
+// projs.forEach(proj => {
+//    proj.setAttribute('title', 'Click for details')
+//    proj.addEventListener('click', activate)
+// });
+
+function activate(){
+   const targetClass = this.id;
+   document.getElementById(targetClass).classList.toggle('show');
+   document.getElementById(targetClass).classList.toggle('collapse');
+}
+
+document.getElementById('aside-expend').addEventListener('click', () => {
+   document.querySelector('aside').classList.toggle('expended');
+   document.querySelector('aside').classList.toggle('shrunk');
+});
